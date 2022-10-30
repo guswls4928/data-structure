0f9a32e6788418b45bcdd91bcec50347f5278aa9
guswls4928 <73646152+guswls4928@users.noreply.github.com>
2022-10-31 00:03:35 +0900

    빠진 숫자 찾기

---
+++ 8주차문제2.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int a, i, j, cnt;
+    scanf("%d", &a); //몇개를 넣을지
+    int *num = malloc(sizeof(int)*a);
+    for (i = 0; i < a - 1; i++) //배열에 넣을 수 입력
+        scanf("%d", &num[i]);
+    for (i = 1; i <= a; i++) {
+        cnt = 0;
+        for (j = 0; j < a - 1; j++) {
+            if (i == num[j]) //모든 배열 값을 탐색 후 있으면 cnt를 true로
+                cnt = 1;
+            continue;
+        }
+        if (cnt)
+            continue;
+        else { //cnt가 true가 아니라면  없는 수를 출력
+            printf("%d", i);
+            break;
+        }
+    }
+}
